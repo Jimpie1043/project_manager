d10f2cdb306038b79193ba2ba9215ea5b5d8573a
FX <fx.leclerc05@gmail.com>
2026-04-24 14:51:11 -0400

    Add config in config.py instead of having it in main init file

--- app/config.py
+++ app/config.py
@@ -1,9 +1,13 @@
 import os
 
 class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+    SQLALCHEMY_DATABASE_URI = None
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     SESSION_COOKIE_SAMESITE = "Lax"
     SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SECURE = os.getenv("ENVIRONMENT") == "production"
 
-    # Both true only in production
-    SESSION_COOKIE_SECURE = os.getenv('ENVIRONMENT') == 'production'
-    FORCE_HTTPS = os.getenv('ENVIRONMENT') == 'production'
+    FORCE_HTTPS = os.getenv("ENVIRONMENT") == "production"
