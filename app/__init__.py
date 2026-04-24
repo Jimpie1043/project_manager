1c237d2e7be7a4f4db5daa78c97945fd9a72c0a5
FX <fx.leclerc05@gmail.com>
2026-04-24 14:49:49 -0400

    Remove config from this file; Clarify extensions

--- app/__init__.py
+++ app/__init__.py
@@ -4,48 +4,45 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 from dotenv import load_dotenv
+from flask_babel import Babel
 
 from app.utils.security import init_security
+from app.config import Config
 
+
+# Assign extensions
 db = SQLAlchemy()
 migrate = Migrate()
 csrf = CSRFProtect()
+babel = Babel()
 
 def create_app(test_config=None):
     load_dotenv()
-    
+
     app = Flask(__name__, instance_relative_config=True)
 
-    app.config.from_mapping(
-    SECRET_KEY=os.getenv("SECRET_KEY"),
-    SQLALCHEMY_DATABASE_URI=f"sqlite:///{os.path.join(app.instance_path, 'app.db')}",
-    SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    DEBUG=os.getenv("FLASK_DEBUG") == "1"
+    app.config.from_object(Config)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        f"sqlite:///{os.path.join(app.instance_path, 'app.db')}"
     )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
+    if test_config:
+        app.config.update(test_config)
 
     os.makedirs(app.instance_path, exist_ok=True)
 
-
-    init_security(app)
     csrf.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
+    babel.init_app(app)
 
+    init_security(app)
 
     from .routes.auth_routes import auth
     from .routes.workspace_routes import workspace
 
     app.register_blueprint(auth)
     app.register_blueprint(workspace)
-
-    app.add_url_rule('/', endpoint='index')
 
     return app
